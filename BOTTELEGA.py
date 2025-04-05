989707edc7e50e75c9f57dd800bf249355232c41
Schexvits_Kirill <oraoraoramudamida20@gmail.com>
2025-04-05 10:44:55 +0300

    Update BOTTELEGA.py
    Пока не сделал

--- BOTTELEGA.py
+++ BOTTELEGA.py
@@ -1,7 +1,9 @@
 import telebot
 from telebot import types
 Bz = telebot.TeleBot('7866982285:AAFOIeFAh7_muLORT3VEL3pOtqB_LsUBIKE')
-a = 0
+
+
+
 def create_keyboard():
     keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     button1 = types.KeyboardButton(".")
@@ -59,7 +61,7 @@ def create_keyboard2():
 
 @Bz.message_handler(commands = ["start"])
 def start(m, res = False):
-    Bz.send_message(m.chat.id, '.', reply_markup = create_keyboard())
+    Bz.send_message(m.chat.id, 'Здравствуйте это бот', reply_markup = create_keyboard())
 
 @Bz.message_handler(func=lambda message: True)
 def handle_messages(message):
